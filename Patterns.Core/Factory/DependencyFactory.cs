8dd276794882c1f1edd89b4215cba710549ea1d5
Adi Thakker <athakker@medsyshcs.com>
2018-06-05 13:47:53 -0400

    Added a simple DependencyFactory.

---
+++ Patterns.Core/Factory/DependencyFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Patterns.Core.Factory
+{
+    public static class DependencyFactory
+    {
+        private static ConcurrentDictionary<Type, Func<object>> factory = new ConcurrentDictionary<Type, Func<object>>();
+
+        public static void Set<TType>(Type key, Func<TType> create) where TType : class, new()
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (create == null)
+                throw new ArgumentNullException(nameof(create));
+
+            factory.AddOrUpdate(key, create, (index, createFunc) => create);
+        }
+
+        public static TResult Get<TResult>()
+        {
+            if (!factory.TryGetValue(typeof(TResult), out Func<object> func))
+                throw new InvalidOperationException($"No dependency registered for {typeof(TResult)}");
+
+            return (TResult)func();
+        }
+    }
+}
---
+++ Patterns.Test/FactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patterns.Core.Factory;
+using Patterns.Infrastructure.Domain.Entities;
+
+namespace Patterns.Test
+{
+    [TestClass]
+    public class FactoryTest
+    {
+        [TestMethod]
+        public void TestDependencyRegistered_IsNotNull()
+        {
+            DependencyFactory.Set(typeof(Log), () => new Log());
+            Assert.IsNotNull(DependencyFactory.Get<Log>());
+        }
+
+        [TestMethod]
+        public void TestDependencyRegistered_IsUpdated()
+        {
+            DependencyFactory.Set(typeof(Log), () => new Log());
+
+            // Simulate Update
+            DependencyFactory.Set(typeof(Log), () => new Log());
+
+            Assert.IsNotNull(DependencyFactory.Get<Log>());
+        }
+    }
+}
