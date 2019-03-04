19f20b443ecd5bbad83e00777eb386e6a9c5a564
Aditya D Thakker <aditya.thakker@gmail.com>
2019-03-03 21:09:25 -0500

    removed usings

--- Patterns.Core/Mediator/CommandHandler.cs
+++ Patterns.Core/Mediator/CommandHandler.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Patterns.Core.Command.Interfaces;
 using Patterns.Core.Mediator.Interfaces;
 
