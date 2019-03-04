37d33cd1ce433e3db965a3dbc06f793b13f3bcba
Aditya D Thakker <aditya.thakker@gmail.com>
2019-03-03 21:14:06 -0500

    started with logic to add dispatchers

--- Patterns.Core/Mediator/CommandDispatcher.cs
+++ Patterns.Core/Mediator/CommandDispatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using Patterns.Core.Command.Interfaces;
 using Patterns.Core.Mediator.Interfaces;
 
@@ -6,6 +7,13 @@ namespace Patterns.Core.Mediator
 {
     public class CommandDispatcher : ICommandDispatcher
     {
+        ConcurrentDictionary<Type, ICommandHandler> dispatcher = new ConcurrentDictionary<Type, ICommandHandler>();
+
+        public CommandDispatcher()
+        {
+            // Hydrate dispatchers
+        }
+
         public TResponse Dispatch<TType, TRequest, TResponse>(TType command, TRequest request)
             where TType : ICommand<TRequest, TResponse>
             where TRequest : class
