d1ad55b18bdebac8ca006edc8c573e8fe98b8289
Aditya D Thakker <aditya.thakker@gmail.com>
2019-03-16 08:56:43 -0400

    started with some basic logging

--- Patterns.Core/Mediator/CommandLogger.cs
+++ Patterns.Core/Mediator/CommandLogger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Patterns.Core.Command.Interfaces;
 using Patterns.Core.Mediator.Interfaces;
 
@@ -6,15 +7,24 @@ namespace Patterns.Core.Mediator
 {
     public class CommandLogger : ICommandHandler
     {
+        public StringBuilder LogStatus { get; private set; }
+
+        public CommandLogger() => LogStatus = new StringBuilder();
+
         public TResponse Handle<TType, TRequest, TResponse>(TRequest request)
             where TType : ICommand<TRequest, TResponse>, new()
             where TRequest : class
         {
             TType command = new TType();
 
-            // TODO: Add Logging behavior
-            var commandInstance = (ICommand <TRequest, TResponse>)Activator.CreateInstance(command.GetType());
-            return commandInstance.Execute(request);
+            LogStatus.AppendLine($"Instantiating command {command.GetType()}");
+
+            var commandInstance = (ICommand<TRequest, TResponse>)Activator.CreateInstance(command.GetType());
+            var result = commandInstance.Execute(request);
+
+            LogStatus.AppendLine($"Executed command {command.GetType()} and got the result {result.ToString()}");
+
+            return result;
         }
     }
 }
