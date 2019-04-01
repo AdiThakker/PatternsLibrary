c84c2539dffa758ab9e48167154ae45d3b93c6fa
Aditya D Thakker <aditya.thakker@gmail.com>
2019-04-01 19:51:07 -0400

    Composite Refactoring

---
+++ Patterns.Core/Mediator/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Patterns.Core.Command.Interfaces;
+using Patterns.Core.Mediator.Interfaces;
+
+namespace Patterns.Core.Mediator
+{
+    public class CommandHandler : ICommandHandler
+    {
+        public TResponse Handle<TType, TRequest, TResponse>(TRequest request)
+            where TType : ICommand<TRequest, TResponse>, new()
+            where TRequest : class
+        {
+            TType command = new TType();
+
+            var commandInstance = (ICommand<TRequest, TResponse>)Activator.CreateInstance(command.GetType());
+            return commandInstance.Execute(request);
+        }
+    }
+}
